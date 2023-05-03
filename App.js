84330c469bca4cbf64b84591b2195d90e9b4d253
Candido Tominaga <candido1212@gmail.com>
2023-05-03 17:09:52 -0300

    Componente ShowImage com passada de parametros (Aula 13)

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 
 class App extends React.Component {
     render() {
@@ -12,9 +12,24 @@ class App extends React.Component {
                 <Text style={{ color: 'red', fontSize: 50, margin: 20}}>Estilo com Cor</Text>
 
                 <Text>{nome}</Text>
+
+                <ShowImage largura={300} altura={300} />
             </View>
         )
     }
 }
+
+class ShowImage extends Component {
+    render() {
+        let image = 'https://img.freepik.com/fotos-gratis/foto-de-grande-angular-de-uma-unica-arvore-crescendo-sob-um-ceu-nublado-durante-um-por-do-sol-cercado-por-grama_181624-22807.jpg?w=1380&t=st=1683144273~exp=1683144873~hmac=a6cef9a00c745f6b31d77db90387d24bcde2cd1c6b4ab47b25a0c70cd41e9d90';
+        return(
+            <Image 
+            source={{ uri: image}} 
+            style={{ width: this.props.largura, height: this.props.altura }}
+            />
+        );
+    }
+}
+
 
 export default App;
