d784dae53801be61ce2256dc4fdca7a00272093e
Candido Tominaga <candido1212@gmail.com>
2023-05-03 17:01:35 -0300

    chamada de variavel no componente (Aula 12)

--- App.js
+++ App.js
@@ -3,11 +3,15 @@ import { View, Text } from "react-native";
 
 class App extends React.Component {
     render() {
+        let nome = 'Fulano de Tal';
+
         return(
             <View>
                 <Text>Ola Mundo</Text>
                 <Text>Componentização</Text>
                 <Text style={{ color: 'red', fontSize: 50, margin: 20}}>Estilo com Cor</Text>
+
+                <Text>{nome}</Text>
             </View>
         )
     }
