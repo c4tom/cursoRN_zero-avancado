28c3916941b2d615dfa30ee76469dc969742455d
Candido Tominaga <candido1212@gmail.com>
2023-05-03 21:54:45 -0300

    Flexbox e alinhamentos (Aula 18)

--- App.js
+++ App.js
@@ -1,39 +1,26 @@
-import React, { Component } from "react";
-import { View, Text } from "react-native";
+/* eslint-disable react-native/no-inline-styles */
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
 
 class App extends React.Component {
-    // https://reactnative.dev/docs/flexbox?language=typescript#align-content
-    render() {
-        return(
-            <View style={{flex:1}}>
-                <View style={{flex:3, backgroundColor:'black'}}>
-                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
-                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
-                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
-                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
-                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
-                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
-                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
-                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
-                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
-                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
-                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
-                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
-                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
-                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
-                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
-                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
-                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
-                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
-                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
-                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
-
-                </View>
-                <View style={{flex:1, backgroundColor:'blue'}}/>
-                <View style={{flex:1, backgroundColor:'yellow'}}/>
-            </View>
-        )
-    }
+  // https://reactnative.dev/docs/flexbox?language=typescript#align-content
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'row',
+          justifyContent: 'center',
+          alignItems: 'flex-end',
+        }}>
+        <View style={{width: 50, height: 50, backgroundColor: 'black'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'blue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'yellow'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'pink'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'violet'}} />
+      </View>
+    );
+  }
 }
 
 export default App;
