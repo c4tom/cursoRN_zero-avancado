158297861144afeb7aa046f5d6d2de53e166d502
Candido Tominaga <candido1212@gmail.com>
2023-05-03 21:23:56 -0300

    Tamanhos Dinamicos/Fixos com Flex (Aula  16)

--- App.js
+++ App.js
@@ -1,41 +1,39 @@
 import React, { Component } from "react";
-import { View, Text, Image, Button, StyleSheet } from "react-native";
+import { View, Text } from "react-native";
 
 class App extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            nome: 'Fulano Beltrano'
-        };
-
-        this.trocarEstado = this.trocarEstado.bind(this);
-    }
-
-    trocarEstado(nome){
-        this.setState({
-            nome: nome
-        })
-    }
-
+    // https://reactnative.dev/docs/flexbox?language=typescript#align-content
     render() {
         return(
-            <View style={styles.container}>
-                <Button color={'blue'} title="Trocar Estado" onPress={() => this.trocarEstado('Beltrano da Silva')} />
-                <Button color={'red'} title="Reset" onPress={() => this.trocarEstado('')} />
-                <Text style={styles.nome}>{this.state.nome}</Text>
+            <View style={{flex:1}}>
+                <View style={{flex:3, backgroundColor:'black'}}>
+                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
+                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
+                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
+                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
+                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
+                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
+                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
+                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
+                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
+                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
+                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
+                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
+                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
+                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
+                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
+                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
+                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
+                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
+                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
+                    <Text style={{color: 'white'}}>Ola mundo, estou aqui</Text>
+
+                </View>
+                <View style={{flex:1, backgroundColor:'blue'}}/>
+                <View style={{flex:1, backgroundColor:'yellow'}}/>
             </View>
         )
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        marginTop: 40
-    },
-    nome: {
-        fontSize: 35
-    }
-});
 
 export default App;
